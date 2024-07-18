5e10972869c498159dd607cb6d8abeb48f3c0583
asiajcrew <asia_jcrew96@yahoo.com>
2024-07-18 07:41:37 +0100

    added event listener to button. Will create a dynamic element to list once clicked

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var buttonEl = document.querySelector("#save-task");
+var taskListEl = document.querySelector("#task-list")
+
+var createTaskHandler = function() {
+    var taskItemEl = document.createElement("li")
+    taskItemEl.textContent = "hello"
+    taskListEl.appendChild(taskItemEl)
+    taskItemEl.className = "task-item"
+}
+
+buttonEl.addEventListener("click", createTaskHandler);~
